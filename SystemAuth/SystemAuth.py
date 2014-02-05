cfd4d6c37a920d6a756e983599f9d905fdb31c39
Torf <tberger.dev@gmail.com>
2014-02-06 00:48:29 +0100

    systemauth plugin

---
+++ SystemAuth/SystemAuth.py
@@ -0,0 +1,35 @@
+# Author: Torf
+# URL: https://github.com/lad1337/XDM
+#
+# This file is part of XDM: eXtentable Download Manager.
+#
+# XDM: eXtentable Download Manager. Plugin based media collection manager.
+# Copyright (C) 2013  Dennis Lutter
+#
+# XDM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# XDM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+from xdm.plugins import System
+from collections import OrderedDict
+
+class SystemConfig(System):
+    identifier = "fr.torf.systemauth"
+    version = "0.1"
+    _config = OrderedDict([
+               ('login_user', ''),
+               ('login_password', '')
+               ])
+
+    config_meta = { 'login_user': {'on_change_actions': ['serverReStart']},
+                    'login_password': {'on_change_actions': ['serverReStart']}
+                  }
