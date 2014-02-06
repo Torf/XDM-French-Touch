abf3bd1bab0bf97ccbd309024c6afb7a1b299f6e
Torf <tberger.dev@gmail.com>
2014-02-06 03:08:37 +0100

    working pwd change feature w/ sha512 encrypt before network

--- System Auth/SystemAuth.py
+++ System Auth/SystemAuth.py
@@ -24,6 +24,7 @@ from collections import OrderedDict
 from xdm import helper
 import cherrypy
 import os.path
+import hashlib
 
 class SystemAuth(System):
     identifier = "fr.torf.systemauth"
@@ -35,45 +36,61 @@ class SystemAuth(System):
     _hidden_config = {'login_password': ''}
 
     def _saveNewPassword(self, pwdfirst, pwdsecond):
-    	if not pwdfirst or not pwdsecond:
-    		return (False, {'callFunction': 'systemauth_' + self.instance + '_addpwdchange', 'functionData': {}}, "You have to fill the two field.")
+        if not pwdfirst or not pwdsecond:
+            return (False, {'callFunction': 'systemauth_' + self.instance + '_addpwdchange', 'functionData': {}}, "You have to fill the two field.")
 
-    	if pwdfirst != pwdsecond:
-    		return (False, {'callFunction': 'systemauth_' + self.instance + '_addpwdchange', 'functionData': {}}, "Password and confirmation aren't identical.")
+        if pwdfirst != pwdsecond:
+            return (False, {'callFunction': 'systemauth_' + self.instance + '_addpwdchange', 'functionData': {}}, "Password and confirmation aren't identical.")
 
-    	self.hc.login_password = pwdfirst
+        newpwd = hashlib.sha512(str(pwdfirst)).hexdigest()
+
+        self.hc.login_password = newpwd
+        
         cherrypy.server.restart()
 
-    	return (True, {'callFunction': 'systemauth_' + self.instance + '_clear', 'functionData': {}}, "Password changed.")
+        return (True, {'callFunction': 'systemauth_' + self.instance + '_clear', 'functionData': {}}, "Password changed.")
+    _saveNewPassword.args = ['pwdfirst','pwdsecond']
 
     def _changePassword(self):
-    	return (True, {}, "")
+        return (True, {'callFunction': 'systemauth_' + self.instance + '_addpwdchange', 'functionData': {}}, "")
+    _changePassword.args = []
 
     def getConfigHtml(self):
         return """<script>
-        		function systemauth_""" + self.instance + """_precrypt() {
+                function systemauth_""" + self.instance + """_precrypt() {
+                    if(typeof systemauth_alreadyinclude == 'undefined') {
+                        systemauth_alreadyinclude = 1;
+                        var src = '/api/rest""" + ('/%s/%s/' % (self.identifier, self.instance)) +  """libsha';
+                        var script = document.createElement('script');
+                        script.src = src;
+                        script.type = 'text/javascript';
+                        (document.getElementsByTagName('HEAD')[0] || document.body).appendChild(script);
+                    }
 
-        		}
+                    
+                    $('input[name="SystemAuth-""" + self.instance + """-pwdfirst"]').val(CryptoJS.SHA3($('input[name="SystemAuth-""" + self.instance + """-pwdfirst"]').val()).toString());
+                    $('input[name="SystemAuth-""" + self.instance + """-pwdsecond"]').val(CryptoJS.SHA3($('input[name="SystemAuth-""" + self.instance + """-pwdsecond"]').val()).toString());
+                }
 
-        		function systemauth_""" + self.instance + """_clear() {
-        			$('#block_""" + helper.idSafe(self.name) + """_pwdchange').remove();
-        		}
+                function systemauth_""" + self.instance + """_clear() {
+                    $('#block_""" + helper.idSafe(self.name) + """_pwdchange').remove();
+                }
 
                 function systemauth_""" + self.instance + """_addpwdchange() {
                     $('#block_""" + helper.idSafe(self.name) + """_pwdchange').remove();
 
                     js = '<div id="block_""" + helper.idSafe(self.name) + """_pwdchange" class="control-group">';
 
-                    js += '<div class="control-group"><label class="control-label" title="">login password</label>';
-					js += '<div class="controls"><input data-belongsto="'+""" + helper.idSafe(self.name) + """+'" name="SystemAuth-""" + self.instance + """-pwdfirst" data-configname="pwdfirst" type="password" value="" onchange="" title="" data-original-title="">'
-					js += '</div></div>';
+                    js += '<div class="control-group"><label class="control-label" title="">new password</label>';
+                    js += '<div class="controls"><input data-belongsto="'+""" + helper.idSafe(self.name) + """+'" name="SystemAuth-""" + self.instance + """-pwdfirst" data-configname="pwdfirst" type="password" value="" onchange="" title="" data-original-title="">'
+                    js += '</div></div>';
 
                     js += '<div class="control-group"><label class="control-label" title="">confirm</label>';
-					js += '<div class="controls"><input data-belongsto="'+""" + helper.idSafe(self.name) + """+'" name="SystemAuth-""" + self.instance + """-pwdsecond" data-configname="pwdsecond" type="password" value="" onchange="" title="" data-original-title="">'
-					js += '</div></div>';
+                    js += '<div class="controls"><input data-belongsto="'+""" + helper.idSafe(self.name) + """+'" name="SystemAuth-""" + self.instance + """-pwdsecond" data-configname="pwdsecond" type="password" value="" onchange="" title="" data-original-title="">'
+                    js += '</div></div>';
 
-					js += '<div class="controls">'
-					js += '<input type="button" class="btn" value="Save new password" onclick="systemauth_""" + self.instance + """_precrypt();pluginAjaxCall(this, \\\'SystemAuth\\\', \\\'""" + self.instance + """\\\', \\\'""" + helper.idSafe(self.name) + """_content\\\', \\\'_saveNewPassword\\\');" data-original-title="" title="" />';
+                    js += '<div class="controls">'
+                    js += '<input type="button" class="btn" value="Save new password" onclick="systemauth_""" + self.instance + """_precrypt();pluginAjaxCall(this, \\\'SystemAuth\\\', \\\'""" + self.instance + """\\\', \\\'""" + helper.idSafe(self.name) + """_content\\\', \\\'_saveNewPassword\\\');" data-original-title="" title="" />';
                     js += '</div></div>';
 
                     $('#""" + helper.idSafe(self.name) + """_content .control-group').last().after(js);
@@ -81,7 +98,7 @@ class SystemAuth(System):
                 </script>
         """
 
-    def _SHA3JS(self):
+    def _libsha(self):
         filepath = os.path.join(self.get_plugin_isntall_path()['path'], 'sha3.js')
         if not os.path.exists(filepath):
             return filepath
@@ -91,11 +108,11 @@ class SystemAuth(System):
             result = f.read()
 
         return result
-    _SHA3JS.rest = True
+    _libsha.rest = True
 
     config_meta = { 'login_user': {'on_change_actions': ['serverReStart']},
-			        'plugin_buttons': {'changePassword': {'action': _changePassword,
-			                                                     'name': 'Change password',
-			                                                     'desc': 'Change the login password'}
-			                           }
-			      }
+                    'plugin_buttons': {'changePassword': {'action': _changePassword,
+                                                                 'name': 'Change password',
+                                                                 'desc': 'Change the login password'}
+                                       }
+                  }
